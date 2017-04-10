63176ce7c151ec6acb49944bae9c66494762f420
鈴木哲平 <suzukiteppei@suzukiteppei-no-MacBook-Pro.local>
2017-04-10 19:45:36 +0900

    fixed

---
+++ demo.lua
@@ -0,0 +1,23 @@
+model_path = 'model120000iteration.t7'
+img_path ='path/to/image'
+_=dofile('ssd.llua')
+dofile('cfg.lua')
+dofile('utils.lua')
+require 'image'
+
+model = torch.load(model_path)
+img = image.load(img_path)
+res = img.clone()
+
+class_list = {'__background__', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
+              'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
+             'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'}
+
+img_r = img[{{1},{},{}}]
+img_b = img[{{3},{},{}}]
+img[{{1},{},{}}] = img_b
+img[{{3},{},{}}] = img_r
+boxes, classes, scores = Detect(model, img, 0.45, 0.6, cfg)
+res = DrawRect(res, boxes[1], classes[1], class_list)
+
+image.savePNG('res.png', res)
